4ebb53765b6fb24ab71548ea50efcb30a33e8bae
Jacky <lyatuujacques25@gmail.com>
2025-06-06 23:54:14 +0300

    consumed endpoints for node status

--- deployment5-frontend/src/App.vue
+++ deployment5-frontend/src/App.vue
@@ -3,9 +3,14 @@
     <div
       class="backdrop-blur-md bg-blue-300/4 !border border-blue-400/20 rounded-md px-6 py-8 shadow-xl text-white w-full mx-auto flex flex-col gap-8"
     >
-      <div class="w-full">
-        <h2 class="text-2xl font-semibold mb-2">Network Monitor</h2>
-        <p class="text-sm">Real-time infrastructure monitoring</p>
+      <div class="w-full flex justify-between">
+        <div>
+          <h2 class="text-2xl font-semibold mb-2">Network Monitor</h2>
+          <p class="text-sm">Real-time infrastructure monitoring</p>  
+        </div>
+        
+        <p class="text-sm">Last Checked: {{  formatReadableDateTime(targetStatusData[1]?.checkedAt)}}</p>
+
       </div>
       <div class="grid grid-cols-4 gap-8">
         <div
@@ -16,7 +21,7 @@
               <v-icon color="gray" icon="mdi-desktop-classic"></v-icon>
               <span>Total nodes</span>
             </h2>
-            <p class="text-3xl font-semibold text-white">{{ generalData.totalNodes }}</p>
+            <p class="text-3xl font-semibold text-white">{{ targetData.length }}</p>
           </div>
         </div>
         <div
@@ -59,38 +64,38 @@
 
     <div class="w-full grid grid-cols-4 gap-4 my-16">
       <div
-        v-for="node in nodesData"
+        v-for="node in targetStatusData"
         :key="node"
-        class="backdrop-blur-md bg-blue-300/4 !border border-blue-400/20 hover:border-blue-400/30 transition-all duration-300 ease-in-out rounded-md !p-6 shadow-xl text-white w-full mx-auto"
+        class="backdrop-blur-md bg-blue-300/4 !border border-blue-400/20 hover:border-blue-400/30 transition-all duration-300 ease-in-out rounded-md !p-6 shadow-xl text-white min-w-full w-fit mx-auto"
       >
-        <div class="w-full flex flex-col">
+        <div class="min-w-full w-fit flex flex-col">
           <h2 class="text-md text-white mb-1 flex items-center gap-3">
             <div
               :class="[
-                node.status == 'warning' ? 'bg-[var(--myyellow)]' : '',
-                node.status == 'critical' ? 'bg-[var(--myred)]' : '',
-                node.status == 'healthy' ? 'bg-[var(--mygreen)]' : '',
+                getStatusLabel(node.httpStatusCode) == 'warning' ? 'bg-[var(--myyellow)]' : '',
+                getStatusLabel(node.httpStatusCode) == 'critical' ? 'bg-[var(--myred)]' : '',
+                getStatusLabel(node.httpStatusCode) == 'healthy' ? 'bg-[var(--mygreen)]' : '',
               ]"
               class="w-3 h-3 rounded-full"
             ></div>
-            <span>{{ node.nodeName }}</span>
+            <span>{{ node.target.name }}</span>
           </h2>
-          <p class="text-sm text-gray-300">{{ node.endpoint }}</p>
+          <p class="text-sm text-gray-300 w-fit">{{ node.target.url }}</p>
           <div
             :class="[
-              node.status == 'warning'
+              node.httpStatusCode>=300 && node.httpStatusCode<=399
                 ? 'border-[var(--myyellow)] bg-[var(--myyellow)]/15 text-[var(--myyellow)]'
                 : '',
-              node.status == 'critical'
+              node.httpStatusCode>=400 && node.httpStatusCode<=599
                 ? 'border-[var(--myred)] bg-[var(--myred)]/15 text-[var(--myred)]'
                 : '',
-              node.status == 'healthy'
+              node.httpStatusCode>=200 && node.httpStatusCode<=299
                 ? 'border-[var(--mygreen)] bg-[var(--mygreen)]/15 text-[var(--mygreen)]'
                 : '',
             ]"
             class="px-4 py-1 rounded-full !border w-fit text-sm font-semibold my-2"
           >
-            {{ node.status }}
+            {{ getStatusLabel(node.httpStatusCode) }}
           </div>
           <div class="grid grid-cols-2 gap-4 my-4">
             <div class="flex flex-col bg-blue-300/4 rounded-lg px-4 py-5">
@@ -98,7 +103,7 @@
                 <v-icon color="#05b8cd" icon="mdi-chart-line-variant"></v-icon>
                 <span>Latency</span>
               </h2>
-              <div class="text-white text-xl font-semibold">{{ node.latency }}</div>
+              <div class="text-white text-xl font-semibold">{{ node.latency }}ms</div>
             </div>
             <div class="flex flex-col bg-blue-300/4 rounded-lg px-4 py-5">
               <h2 class="text-sm text-gray-300 mb-1 flex items-center gap-2">
@@ -122,47 +127,117 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import axios from "axios"
 
-const generalData = {
-  totalNodes: 4,
-  healthy: 2,
-  warning: 1,
-  critical: 1,
+const generalData = ref({
+  healthy: 0,
+  warning: 0,
+  critical: 0,
+})
+
+  // {
+  //   nodeName: 'Frontend 4',
+  //   status: 'healthy',
+  //   latency: '45ms',
+  //   ssl: '44 days',
+  //   uptime: '97.49%',
+  //   endpoint: '172.12.12.23:8003',
+  // },
+
+const targetData = ref([])
+const targetStatusData = ref([])
+
+const getTargetsCountByStatus = ()=>{
+   for(const key of Object.keys(targetStatusData.value)){
+      let targetStatus =  targetStatusData.value[key]
+
+      console.log(targetStatus.httpStatusCode, "status mate");
+      
+
+      if(getStatusLabel(targetStatus.httpStatusCode) == 'warning') generalData.value.warning++
+      if(getStatusLabel(targetStatus.httpStatusCode) == 'healthy') generalData.value.healthy++
+      if(getStatusLabel(targetStatus.httpStatusCode) == 'critical') generalData.value.critical++
+   }
 }
 
-const nodesData = [
-  {
-    nodeName: 'Frontend 1',
-    status: 'warning',
-    latency: '45ms',
-    ssl: '44 days',
-    uptime: '97.49%',
-    endpoint: '172.12.12.23:8000',
-  },
-  {
-    nodeName: 'Frontend 2',
-    status: 'healthy',
-    latency: '45ms',
-    ssl: '44 days',
-    uptime: '97.49%',
-    endpoint: '172.12.12.23:8001',
-  },
-  {
-    nodeName: 'Frontend 3',
-    status: 'critical',
-    latency: '45ms',
-    ssl: '44 days',
-    uptime: '97.49%',
-    endpoint: '172.12.12.23:8002',
-  },
-  {
-    nodeName: 'Frontend 4',
-    status: 'healthy',
-    latency: '45ms',
-    ssl: '44 days',
-    uptime: '97.49%',
-    endpoint: '172.12.12.23:8003',
-  },
-]
+
+const getStatusLabel = (httpStatusCode)=>{
+  
+  if(httpStatusCode>=300 && httpStatusCode<=399) return 'warning'
+
+  if(httpStatusCode>=400 && httpStatusCode<=599) return "critical"
+
+  if(httpStatusCode>=200 && httpStatusCode<=299) return "healthy"
+
+}
+
+const getTargetData = async()=>{  
+  await axios.get(import.meta.env.VITE_APP_URL + "/target/all")
+  .then((res)=>{
+    if(res?.status!=200){
+      console.error("Failed to Load Node Data")
+      return
+    }
+    else {
+      targetData.value = res.data
+    }
+  })
+}
+
+const getStatusForTargets = async()=>{
+  await axios.get(import.meta.env.VITE_APP_URL + "/status/all")
+  .then((res)=>{
+    if(res?.status!=200){
+      console.error("Failed to Status Data")
+      return
+    }
+    else {
+      targetStatusData.value = res.data
+    }
+  })
+}
+
+const getStatusForTarget = async(id)=>{
+  await axios.get(import.meta.env.VITE_APP_URL + `/status/all/${id}`)
+  .then((res)=>{
+    if(res?.status!=200){
+      console.error("Failed to Status Data")
+      return
+    }
+    else {
+      //targetData.value = res.data
+    }
+  })  
+}
+
+const getStatusForEachTarget = async()=>{
+  targetData.value.forEach((target,index)=>{
+
+  })
+}
+
+function formatReadableDateTime(isoString) {
+  const date = new Date(isoString);
+
+  if (isNaN(date)) return "Invalid date";
+
+  const options = {
+    year: "numeric",
+    month: "long",   // e.g., June
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  return date.toLocaleString(undefined, options);
+}
+
+onMounted(async()=>{
+  await getTargetData()
+  await getStatusForTargets()
+  await getStatusForEachTarget()
+  getTargetsCountByStatus()
+})
 </script>
